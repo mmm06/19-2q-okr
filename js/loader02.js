3ad6ea7bf0851baca6caf377bdf8192b5fb589df
m_fukahori <m_fukahori@novitanet.com>
2019-12-05 18:30:02 +0900

    Update loader 191205

--- js/loader02.js
+++ js/loader02.js
@@ -9,21 +9,39 @@ $(function(){
     var oneLoadRatio = 100 / imgLen; // 画像1枚あたりの読み込み進捗率
     var loadRatio = 0; // 全体の進捗率
 
-    for(var i = 0; i < imgLen; i++) {
-        $(images[i]).on('load', function(){
+    // for(var i = 0; i < imgLen; i++) {
+    //     $(images[i]).on('load', function(){
+    //         // 画像を読み込むごとに進捗率が上がる
+    //         loadRatio += oneLoadRatio;
+    //         console.log(Math.round(loadRatio));
+    //         // 読み込み進捗率の値を四捨五入して、整数で表示
+    //         loadRatioArea.text(Math.round(loadRatio));
+    //         // 画像の読み込みが100％に到達するまで待ってから、ローディング画面を非表示
+    //         if(loadRatio >= 100) {
+    //             setTimeout(function(){
+    //                 stopLoader(loaderBg02, loader02);
+    //             }, 500);
+    //         }
+    //     });
+    // }
+
+    images.each(function(){
+        var src = $(this).attr('src');
+        $('<img>').attr('src', src).on('load', function(){
             // 画像を読み込むごとに進捗率が上がる
             loadRatio += oneLoadRatio;
-            console.log(Math.round(loadRatio));
-            // 読み込み進捗率の値を四捨五入して、整数で表示
-            loadRatioArea.text(Math.round(loadRatio));
-            // 画像の読み込みが100％に到達するまで待ってから（1秒ほど）、ローディング画面を非表示
-            if(loadRatio >= 100) {
-                setTimeout(function(){
-                    stopLoader(loaderBg02, loader02);
-                }, 500);
-            }
         });
-    }
+    });
+    setInterval(function(){
+        // 読み込み進捗率の値を四捨五入して、整数で表示
+        loadRatioArea.text(Math.round(loadRatio));
+        // 画像の読み込みが100％に到達するまで待ってから、ローディング画面を非表示
+        if(loadRatio >= 100) {
+            setTimeout(function(){
+                stopLoader(loaderBg02, loader02);
+            }, 500);
+        }
+    }, 1);
 });
 
 function stopLoader(loaderBg, loader) {
